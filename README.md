a7bfb03076b005f3ffc90687b98560f9b8ab60d9
jdeleonardis <jdeleonardis@hotmail.com>
2020-05-01 09:35:20 -0400

    fixed createreadme.js so that badges would display correctly

--- README.md
+++ README.md
@@ -1,3 +1,37 @@
 
-    ![Badge](https://img.shields.io/github/v/tag/jdeleonardis/homework_9_readme_generator?style=plastic)
-    
+
+![Badge](https://img.shields.io/github/v/tag/jdeleonardis/homework_9_readme_generator?style=plastic)
+![Badge](https://img.shields.io/github/v/release/jdeleonardis/homework_9_readme_generator?color=blue&include_prereleases&style=plastic)
+![Badge](https://img.shields.io/github/issues-closed-raw/jdeleonardis/homework_9_readme_generator?color=success&style=plastic)
+![Badge](https://img.shields.io/github/issues-raw/jdeleonardis/homework_9_readme_generator?color=critical&style=plastic)
+
+# Project Title
+jdeleonardis
+
+## GitHub Repository
+homework_9_readme_generator
+
+## Project Description
+description of something amazing that is something amazing
+
+## Project Installation
+very carefully
+
+## Project Usage
+also very carefully
+
+## Project Contributors
+jst me!
+
+## Tests For This Project
+nope
+
+## License
+MIT
+
+## GitHub Email Address
+jdeleonardis@hotmail.com
+
+## Profile Picture
+![Avatar](https://avatars1.githubusercontent.com/u/58078950?v=4)
+
--- utils/createreadme.js
+++ utils/createreadme.js
@@ -3,8 +3,43 @@ const buildReadMe = (answers,gitHubStuff) => {
     // I am loyal to ${answers.user}.`;
 
     const readMeFile = `
-    ![Badge](https://img.shields.io/github/v/tag/${answers.user}/${answers.repo}?style=plastic)
-    `;
+
+![Badge](https://img.shields.io/github/v/tag/${answers.user}/${answers.repo}?style=plastic)
+![Badge](https://img.shields.io/github/v/release/${answers.user}/${answers.repo}?color=blue&include_prereleases&style=plastic)
+![Badge](https://img.shields.io/github/issues-closed-raw/${answers.user}/${answers.repo}?color=success&style=plastic)
+![Badge](https://img.shields.io/github/issues-raw/${answers.user}/${answers.repo}?color=critical&style=plastic)
+
+# Project Title
+${answers.user}
+
+## GitHub Repository
+${answers.repo}
+
+## Project Description
+${answers.description}
+
+## Project Installation
+${answers.install}
+
+## Project Usage
+${answers.usage}
+
+## Project Contributors
+${answers.contribution}
+
+## Tests For This Project
+${answers.tests}
+
+## License
+${answers.license}
+
+## GitHub Email Address
+${gitHubStuff.email}
+
+## Profile Picture
+![Avatar](${gitHubStuff.avatar_url})
+
+`;
 
     return readMeFile;
 
